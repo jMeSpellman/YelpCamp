b87b8caa98c152897861c7aa4f5b93089bd32690
James <jatrill@spellmanhv.co.uk>
2021-11-10 13:57:47 +0000

    section52_517_completed

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,6 +28,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
         const campground = new Campground(req.body.campground);
+        campground.author = req.user._id;
         await campground.save();
         req.flash('success', 'Successfully made a new campground!');
         res.redirect(`/campgrounds/${campground._id}`);
@@ -36,7 +37,8 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 // http://localhost:3000/campgrounds/610a8786c663b543b0a347c7
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    console.log(campground);
     if(!campground){
         req.flash('error','Cannot Find that campground');
         return res.redirect('/campgrounds');
