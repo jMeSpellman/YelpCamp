4122b3f742039199ad3f1e04ce42581f03064a4e
jMeSpellman <88663096+jMeSpellman@users.noreply.github.com>
2021-12-13 13:39:57 +0000

    section54_535_completed

--- seeds/index.js
+++ seeds/index.js
@@ -22,14 +22,24 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random()*20) +10;
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author: '61813d00b65f5a5ba0bb509f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt id velit, blanditiis tenetur laborum dignissimos laboriosam ipsam, facilis odit magnam similique. Quas omnis distinctio sit repudiandae deserunt, similique obcaecati id!',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dwlklfqyu/image/upload/v1639055923/YelpCamp/jesz8qb3xazxalz1zjwd.png',
+                    filename: 'YelpCamp/jesz8qb3xazxalz1zjwd'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwlklfqyu/image/upload/v1638969240/YelpCamp/sqxhjrweuqdava0fxeco.jpg',
+                    filename: 'YelpCamp/sqxhjrweuqdava0fxeco'
+                }
+            ]
+          
         })
         await camp.save();
     }
